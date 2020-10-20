7a224518a0bb6cba88f1e9abb8c10b3b4f2ec92a
Daniil <daniel217032001@gmail.com>
2020-10-20 22:26:03 +0300

    add example

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
-#include "libraries/example.h"
+#include "libraries/Stack.h"
 
 using std::cout;
 
 int main() {
-    example();
+    auto stack = new Stack<int>;
+    stack->push(1);
+    stack->push(2);
+    stack->push(3);
+    stack->push(4);
+    stack->push(5);
+    stack->push(6);
+    stack->push(7);
 
+    while (!stack->is_empty()) {
+        cout << stack->pop() << " ";
+    }
     return 0;
 }
